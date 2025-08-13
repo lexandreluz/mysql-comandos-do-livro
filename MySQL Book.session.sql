0bc623c1b216940cdc3f63b03c70953affbf8e5d
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2025-08-13 18:48:04 +0000

    terminando o capítulo 3

--- MySQL Book.session.sql
+++
@@ -1,8 +0,0 @@
-
-
-/* Adicionar chave estrangeira de comvenda para comvende */
-ALTER TABLE comvenda ADD CONSTRAINT fk_comprodu_comvende
-FOREIGN KEY(n_numevende)
-REFERENCES comvende(n_numevende)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
